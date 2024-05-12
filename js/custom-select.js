699b709fdc01fea36fc4f735782498c2b053c532
compact_pie7 <danilovlive5580@gmail.com>
2024-05-12 23:17:09 +0300

    still on work with custom select

--- js/custom-select.js
+++ js/custom-select.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const modalSelect = document.querySelector('.modal__select');
-    const defaultSelect = modalSelect.querySelector('[data-select="default-select"]');
-    console.log(defaultSelect);
+    const selectOptionList = modalSelect.querySelector('.modal__option-list')
+    const selectOptions = modalSelect.querySelector('[data-select="select-options"]');
+    console.log(selectOptionList);
 
-    function openCustomSelect(select) {
-        if(select.classList.contains('open')) {
-            select.classList.remove('open');
+    function openCustomSelect(selectList) {
+        if(!selectList) return;
+        const selectHeight = selectList.querySelector('[data-select="select-options"]').offsetHeight;
+        
+        selectList.style.maxHeight = 0;
+
+        if(selectList.classList.contains('open')) {
+            selectList.style.maxHeight = '0';
+            selectList.classList.remove('open');
         } else {
-            select.classList.add('open');
+            selectList.style.maxHeight = `${selectHeight}px`;
+            selectList.classList.add('open');
         }
     }
     
     if(modalSelect) {
         modalSelect.addEventListener('click', () => {
-            openCustomSelect(modalSelect);
+            openCustomSelect(selectOptionList);
+            console.log('select clicked');
         })
     }
 });
