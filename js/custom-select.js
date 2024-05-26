f377c373b7f19e278dc94e107e4fd10b85e97dd8
compact_pie7 <danilovlive5580@gmail.com>
2024-05-26 20:06:51 +0300

    modal list animation and stylization

--- js/custom-select.js
+++ js/custom-select.js
@@ -2,24 +2,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalSelect = document.querySelector('.modal__select');
     const selectOptionList = modalSelect.querySelector('.modal__option-list')
     const selectOptions = modalSelect.querySelector('[data-select="select-options"]');
-    console.log(selectOptionList);
+    const modalOption = modalSelect.querySelector('.modal__option');
+    console.log(modalOption)
 
     function openCustomSelect(selectList) {
-        if(!selectList) return;
-        const selectHeight = selectList.querySelector('[data-select="select-options"]').offsetHeight;
-        
-        selectList.style.maxHeight = 0;
+        if (!selectList) return;
+        const selectHeight = document.querySelector('[data-select="select-options"]').offsetHeight;
 
-        if(selectList.classList.contains('open')) {
-            selectList.style.maxHeight = '0';
+        // selectList.style.maxHeight = 0;
+
+        if (selectList.classList.contains('open')) {
             selectList.classList.remove('open');
+            selectList.style.height = '0';
+            selectList.style.border = 'none';
+
+            modalOption.classList.remove('modal__option_arrow-45')
+            modalOption.classList.add('modal__option_arrow-225')
+            console.log(modalOption)
         } else {
-            selectList.style.maxHeight = `${selectHeight}px`;
             selectList.classList.add('open');
+            selectList.style.height = `${selectHeight}px`;
+            selectList.style.border = '2px solid #c8c5d1';
+            selectList.style.borderTop = 'none';
+            
+            modalOption.classList.remove('modal__option_arrow-225')
+            modalOption.classList.add('modal__option_arrow-45')
+            console.log(modalOption)
         }
     }
-    
-    if(modalSelect) {
+
+    if (modalSelect) {
         modalSelect.addEventListener('click', () => {
             openCustomSelect(selectOptionList);
             console.log('select clicked');
