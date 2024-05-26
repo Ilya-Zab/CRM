3f39b6db29ae90011f4852d006d9657fc7dfca13
Ilya-Zab <anno.201543@icloud.com>
2024-05-26 20:41:15 +0200

    empty

--- js/custom-select.js
+++ js/custom-select.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () =>
+{
     const modalSelect = document.querySelector('.modal__select');
     const selectOptionList = modalSelect.querySelector('.modal__option-list')
     const selectOptions = modalSelect.querySelector('[data-select="select-options"]');
     const modalOption = modalSelect.querySelector('.modal__option');
     console.log(modalOption)
 
-    function openCustomSelect(selectList) {
+    function openCustomSelect(selectList)
+    {
         if (!selectList) return;
         const selectHeight = document.querySelector('[data-select="select-options"]').offsetHeight;
 
-        // selectList.style.maxHeight = 0;
-
-        if (selectList.classList.contains('open')) {
+        if (selectList.classList.contains('open'))
+        {
             selectList.classList.remove('open');
             selectList.style.height = '0';
             selectList.style.border = 'none';
@@ -19,20 +20,23 @@ document.addEventListener('DOMContentLoaded', () => {
             modalOption.classList.remove('modal__option_arrow-45')
             modalOption.classList.add('modal__option_arrow-225')
             console.log(modalOption)
-        } else {
+        } else
+        {
             selectList.classList.add('open');
             selectList.style.height = `${selectHeight}px`;
             selectList.style.border = '2px solid #c8c5d1';
             selectList.style.borderTop = 'none';
-            
+
             modalOption.classList.remove('modal__option_arrow-225')
             modalOption.classList.add('modal__option_arrow-45')
             console.log(modalOption)
         }
     }
 
-    if (modalSelect) {
-        modalSelect.addEventListener('click', () => {
+    if (modalSelect)
+    {
+        modalSelect.addEventListener('click', () =>
+        {
             openCustomSelect(selectOptionList);
             console.log('select clicked');
         })
